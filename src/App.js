4e29b932bb11cb5d7c2f3d66d7510cb099eb1a0a
Michael Anthony Cabrera <31862427+mcabrera9@users.noreply.github.com>
2025-08-05 01:13:48 -0500

    moved astro contents from home to astro page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
-import Astro from './Astro';
+import Astro from './pages/Astro';
 import Home from './Home';
 import Counter from './Counter';
 
@@ -12,7 +12,7 @@ function App() {
       <div style={{ padding: '1rem' }}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/astro" element={<Astro />} />
+          <Route path="/pages/astro" element={<Astro />} />
           <Route path="/counter" element={<Counter />} />
         </Routes>
       </div>
---
+++ src/pages/CounterPage.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import '../App.css';
+import '../Counter';
+
+function CounterPage() {
+     //wip
+}
